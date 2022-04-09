c9a010aa02c3e56bbc0cd41ce3a9d924f5bab4a0
forestfamily4 <54976891+forestfamily4@users.noreply.github.com>
2022-04-09 19:53:07 +0900

    プロジェクト ファイルを追加します。

---
+++ ytdownloader/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ytdownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(615, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(25, 223);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(556, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 135);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(520, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "動画のURL";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(25, 179);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(168, 19);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "音声形式でダウンロード";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(25, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(405, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(457, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 50);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "フォルダの選択";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "フォルダのパス";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "mp3",
+            "ogg",
+            "wav"});
+            this.comboBox1.Location = new System.Drawing.Point(207, 177);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.Text = "mp3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 299);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "youtubedownloader";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ ytdownloader/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Windows.Forms;
+using VideoLibrary;
+using System.IO;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Net.Http;
+using System.IO.Compression;
+using System.Reflection;
+
+namespace ytdownloader
+{
+    public partial class Form1 : Form
+    {
+        private string filepath;
+        public Form1()
+        {
+            ecodecotool();
+            InitializeComponent();
+            textBox2.Text = Properties.Settings.Default.defaultpath;
+           checkBox1.Checked = Properties.Settings.Default.defaultmusicbool;
+            comboBox1.Text = Properties.Settings.Default.defaultmusictype;
+           textBox1.Text = Properties.Settings.Default.defaulturl;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "出力するフォルダーを開いてください。";
+            if (dialog.ShowDialog(this) == DialogResult.Cancel)
+            {
+                return;
+            }
+            string folderpath=dialog.SelectedPath;
+            dialog.Dispose();
+            textBox2.Text=folderpath;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Properties.Settings.Default.defaultpath = textBox2.Text;
+            Properties.Settings.Default.defaultmusicbool = checkBox1.Checked;
+            Properties.Settings.Default.defaultmusictype = comboBox1.Text;
+            Properties.Settings.Default.defaulturl = textBox1.Text;
+            Properties.Settings.Default.Save();
+            Application.Exit();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!Directory.Exists(textBox2.Text)) { MessageBox.Show("パスが不正です。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error); return; }
+                await savevideo(textBox1.Text, textBox2.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+           
+            if (checkBox1.Checked)
+            {
+                try
+                {
+                    string kakutyousi = comboBox1.Text;
+                    string num = "";
+                    if (kakutyousi == "mp3")
+                    {
+                        num = "1";
+                    }
+                    else if (kakutyousi == "wav")
+                    {
+                        num = "2";
+                    }
+                    else if (kakutyousi == "ogg")
+                    {
+                        num = "3";
+                    }
+                    else
+                    {
+                        MessageBox.Show("音声形式が不正です。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    File.Delete(Application.LocalUserAppDataPath + @"\EcoDecoTooL114\EcoDecoTooL.ini");
+                    File.Copy(Application.LocalUserAppDataPath + @"\EcoDecoTooL114\temp" + num + ".ini", Application.LocalUserAppDataPath + @"\EcoDecoTooL114\EcoDecoTooL.ini");
+                    ProcessStartInfo info = new ProcessStartInfo();
+                    info.FileName = Application.LocalUserAppDataPath + @"\EcoDecoTooL114\EcoDecoTooL.exe";
+                    info.Arguments = "\"" + filepath + "\"";
+                    info.WindowStyle = ProcessWindowStyle.Hidden;
+                    Process p = Process.Start(info);
+                    p.WaitForExit();
+                    File.Delete(filepath);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        async 
+        Task
+savevideo(string link,string path)
+        {
+            var youtube = YouTube.Default;
+            var video = youtube.GetVideo(link);
+            var client = new HttpClient();
+            long? totalByte = 0;
+            
+            string fileresultpath = path + @"\" + video.Title;
+            fileresultpath=delinvalidchar(fileresultpath);
+            filepath = fileresultpath;
+            Console.WriteLine(fileresultpath);
+            using (Stream output = File.OpenWrite(fileresultpath))
+            {
+                using (var request = new HttpRequestMessage(HttpMethod.Head, video.Uri))
+                {
+                    totalByte = client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).Result.Content.Headers.ContentLength;
+                }
+                using (var input = await client.GetStreamAsync(video.Uri))
+                {
+                    byte[] buffer = new byte[16 * 1024];
+                    int read;
+                    int totalRead = 0;
+                    while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        output.Write(buffer, 0, read);
+                        totalRead += read;
+                        progressBar1.Value = (int)((totalRead / (double)totalByte)*100);
+                        Console.WriteLine(totalRead);
+                    }
+                }
+            }
+        }
+
+        string delinvalidchar(string content)
+        {
+            while (true)
+            {
+                char[] invalidChars = System.IO.Path.GetInvalidPathChars();
+                int num = content.IndexOfAny(invalidChars);
+                Console.WriteLine(num);
+                if (num == -1) { break; }
+                else
+                {
+                    Console.WriteLine(content);
+                  content= content.Remove(num,1);
+                    Console.WriteLine(content);
+                }
+            }
+            if (content.Substring(content.Length - 4) != ".mp4") { content += ".mp4"; }
+            return content;
+        }
+
+        void ecodecotool()
+        {
+             bool isfirst=Properties.Settings.Default.isfirst;
+           
+            Console.WriteLine(isfirst);
+           
+            if (isfirst)
+            {
+                try
+                {
+
+                    byte[] ecodecotoolzip = ytdownloader.Properties.Resources.EcoDecoTooL114;
+                    ZipArchive zipArchive = new ZipArchive(new MemoryStream(ecodecotoolzip));
+                    zipArchive.ExtractToDirectory(Application.LocalUserAppDataPath);
+                    Properties.Settings.Default.isfirst = false;
+                    Properties.Settings.Default.Save();
+                    
+                }
+                catch { 
+                }
+            }
+        }
+    }
+}
